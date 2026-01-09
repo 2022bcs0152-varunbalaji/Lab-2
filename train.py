f6d04fd19348087aaeacaabb306d1b1417190fd4
Varun Balaji <varunbalaji06@gmail.com>
2026-01-09 22:13:19 +0530

    EXP-06 Random Forest 100 trees depth 15, split 80/20

--- train.py
+++ train.py
@@ -38,9 +38,11 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 model = RandomForestRegressor(
-    n_estimators=50,
+    n_estimators=100,
+    max_depth=15,
     random_state=42
 )
+
 
 
 model.fit(X_train, y_train)
